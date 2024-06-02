874dca07fb40de8db8d357cc7c2ad147514b1c50
youyoumu <19760884+youyoumu@users.noreply.github.com>
2024-06-02 16:02:57 +0700

    Complete #2

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import { Application, Assets } from 'pixi.js'
+import { Application, Assets, Sprite } from 'pixi.js'
 
 const app = new Application()
 async function setup() {
@@ -44,5 +44,19 @@ async function preload() {
   await Assets.load(assets)
 }
 
-setup()
-preload()
+;(async () => {
+  await setup()
+  await preload()
+  const background = Sprite.from('background')
+  background.anchor.set(0.5)
+  if (app.screen.width > app.screen.height) {
+    background.width = app.screen.width * 1.2
+    background.scale.y = background.scale.x
+  } else {
+    background.height = app.screen.height * 1.2
+    background.scale.x = background.scale.y
+  }
+  background.x = app.screen.width / 2
+  background.y = app.screen.height / 2
+  app.stage.addChild(background)
+})()
